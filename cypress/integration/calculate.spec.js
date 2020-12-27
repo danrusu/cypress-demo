f24a06af2fe36229986dbb84337a830d3a2e9f3f
Dan Rusu <dangingko@yahoo.com>
2020-12-27 02:06:42 +0200

    added intercept tests

--- cypress/integration/calculate.spec.js
+++ cypress/integration/calculate.spec.js
@@ -1,4 +1,13 @@
-describe('qatools.ro calculate.app test', () => {
+const calculateTest = (nr1, nr2, operation, expectedResultString) =>
+  it(`${operation} result of ${nr1} and ${nr2} should be ${expectedResultString}`, () => {
+    cy.get('#nr1').clear().type(nr1);
+    cy.get('#nr2').clear().type(nr2);
+    cy.get('select').select(operation);
+    cy.get('button[name="calculate"]').click();
+    cy.get('[data-qa-test="result"]').should('have.text', expectedResultString);
+  });
+
+describe('Test qatools.ro calculateApp', () => {
   beforeEach(() => {
     cy.visit('http://qatools.ro');
     cy.url().should('eq', 'http://qatools.ro/');
@@ -7,23 +16,7 @@ describe('qatools.ro calculate.app test', () => {
     cy.title().should('eq', 'calculate.app');
   });
 
-  const calculateTest = (nr1, nr2, operation, expectedResultString) => {
-    cy.get('#nr1').clear().type(nr1);
-    cy.get('#nr2').clear().type(nr2);
-    cy.get('select').select(operation);
-    cy.get('button[name="calculate"]').click();
-    cy.get('[data-qa-test="result"]').should('have.text', expectedResultString);
-  };
-
-  it('Sum of 100 and -100 should be 0', () => {
-    calculateTest(100, -100, 'SUM', '0');
-  });
-
-  it('Sum of 3 and 7 should be 21', () => {
-    calculateTest(3, 7, 'MULTIPLICATION', '21');
-  });
-
-  it('Division of 10 and 2 should be 5', () => {
-    calculateTest(1, 50, 'DIVISION', '0.02');
-  });
+  calculateTest(100, -100, 'SUM', '0');
+  calculateTest(3, 7, 'MULTIPLICATION', '21');
+  calculateTest(1, 50, 'DIVISION', '0.02');
 });
