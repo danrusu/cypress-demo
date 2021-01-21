c31ed5a1190a1b9027542355c8b59d8b4d99185e
Dan Rusu <dangingko@yahoo.com>
2021-01-21 18:53:34 +0200

    syntax fixes

--- cypress/integration/SimpleCalculator/operation.js
+++ cypress/integration/SimpleCalculator/operation.js
@@ -1,14 +1,13 @@
 // those are global - not realy needed
 //import { Given, When, Then } from 'cypress-cucumber-preprocessor/steps';
 
-When(`User types {string} in Number1 input`, number1 => {
-  typeToInput('#nr1', number1);
-});
+When(`User types {string} in Number1 input`, number1 =>
+  typeToInput('#nr1', number1)
+);
 
-And(`User types {string} in Number2 input`, number2 => {
-  typeToInput('#nr2', number2);
-  e;
-});
+And(`User types {string} in Number2 input`, number2 =>
+  typeToInput('#nr2', number2)
+);
 
 And(`User selects {string} operation from dropdown`, operation =>
   cy.get('select').select(operation)
