3b07b53d524a4e9649afcc63052093da874c2dd1
Dan RUSU <danginkgo@yahoo.com>
2021-04-24 00:55:49 +0300

    wait for cookie to exist - weird version

---
+++ cypress/integration/tricky/cookie-test.js
@@ -0,0 +1,27 @@
+describe('Cookie test', () => {
+  
+  it('Wait for cookie', () => {
+    cy.visit('http://qatools.ro/cookie');
+
+    const waitForCookie = (cookieName, timeout) => {
+      cy.log(`Wait ${timeout} seconds for cookie '${cookieName}' to exist`);
+      let count = 0;
+      const wait = options => {
+        cy.getCookie(cookieName, options).then(cookie => {
+          if (cookie === null && count * 500 < timeout) {
+            count++;
+            cy.wait(500);
+            wait({ log: false });
+          }
+          return;
+        });
+      };
+      wait();
+      cy.getCookie('id').should('exist');
+    };
+
+    waitForCookie('id', 2000);
+
+    cy.getCookie('id').should('have.property', 'value', '1111');
+  });
+});
