999a5579cb8f1617bb78f5f010ab360c7299ae04
Dan Rusu <danginkgo@yahoo.com>
2022-01-03 12:05:13 +0200

    Update env-test.js

--- cypress/integration/tricky/env-test.js
+++ cypress/integration/tricky/env-test.js
@@ -1,8 +1,10 @@
 describe('Environment variables test suite', () => {
   it('Enviroment vars test 1', () => {
-    console.log('Environment:');
+    console.log(`Environment: ${JSON.stringify(Cypress.env(), null, 2)}`);
     console.log('baseUrl: ' + Cypress.env('baseUrl'));
     console.log('viewport: ' + Cypress.env('viewport'));
+    // to console output
+    console.log(`Environment: ${JSON.stringify(Cypress.env(), null, 2)}`); 
     cy.log('baseUrl: ' + Cypress.env('baseUrl'));
     cy.log('viewport: ' + Cypress.env('viewport'));
   });
