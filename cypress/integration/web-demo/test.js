08e9f77395da70114c5551991d5b54cb8d815b8c
Dan RUSU <danginkgo@yahoo.com>
2022-01-10 16:50:25 +0200

    x

--- cypress/integration/web-demo/test.js
+++ cypress/integration/web-demo/test.js
@@ -1,5 +1,5 @@
-describe('TServer demo', () => {
-  it('Should be able open homepage', () => {
+describe('Web server demo', () => {
+  it('Should open the homepage', () => {
     cy.visit('/');
     cy.get('[data-test=title]').should('have.text', 'Web Demo');
   });
